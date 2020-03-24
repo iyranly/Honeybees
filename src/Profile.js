bcd4c035eeebc2343b81c248074b7de9316c6d8d
yixiu <shanghai.xy@163.com>
2020-03-25 01:46:46 +0800

    存、取列表数据

--- src/Profile.js
+++ src/Profile.js
@@ -18,7 +18,61 @@ export default class Profile extends Component {
   	  	  return avatarFallbackImage;
   	  	},
   	  },
+      newJobLog:"",
+      jobLogIndex:0,
+      isLoading:true,
+      jobLogs: []
   	};
+  }
+
+  handleNewJobLogChange(event) {
+    this.setState({newJobLog: event.target.value})
+  }
+
+  handleNewJobLogSubmit(event) {
+    this.saveNewStatus(this.state.newJobLog)
+    this.setState({
+      newJobLog: ""
+    })
+  }
+
+  saveNewStatus(jobLogText) {
+    const { userSession } = this.props
+    let jobLogs = this.state.jobLogs
+
+    let jobLog = {
+      id: this.state.jobLogIndex++,
+      log: jobLogText.trim(),
+      created_at: Date.now()
+    }
+
+    jobLogs.unshift(jobLog)
+    const options = { encrypt: false }
+    userSession.putFile('jobLogs.json', JSON.stringify(jobLogs), options)
+      .then(() => {
+        this.setState({
+          jobLogs: jobLogs
+        })
+      })
+  }
+
+  fetchData() {
+    const { userSession } = this.props
+    this.setState({ isLoading: true })
+    const options = { decrypt: false }
+    userSession.getFile('jobLogs.json', options)
+      .then((file) => {
+        var jobLogs = JSON.parse(file || '[]')
+        this.setState({
+          person: new Person(userSession.loadUserData().profile),
+          username: userSession.loadUserData().username,
+          statusIndex: jobLogs.length,
+          jobLogs: jobLogs,
+        })
+      })
+      .finally(() => {
+        this.setState({ isLoading: false })
+      })
   }
 
   render() {
@@ -29,17 +83,47 @@ export default class Profile extends Component {
       <div className="panel-welcome" id="section-2">
         <div className="avatar-section">
           <img src={ person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage } className="img-rounded avatar" id="avatar-image" alt=""/>
+
+          <h1>你好, <span id="heading-name">{ person.name() ? person.name() : 'Nameless Person' }</span>!</h1>
+          <br/>
+          <br/>
+
+          <p className="lead">
+            <button
+              className="btn btn-primary btn-lg"
+              id="signout-button"
+              onClick={ handleSignOut.bind(this) }
+            >
+              退出当前账号
+            </button>
+          </p>
         </div>
-        <h1>Hello, <span id="heading-name">{ person.name() ? person.name() : 'Nameless Person' }</span>!</h1>
-        <p className="lead">
-          <button
-            className="btn btn-primary btn-lg"
-            id="signout-button"
-            onClick={ handleSignOut.bind(this) }
-          >
-            Logout
-          </button>
-        </p>
+        <div className="new-jobLog">
+          <div className="col-md-12">
+               <textarea className="input-log"
+                         value={this.state.newJobLog}
+                         onChange={e => this.handleNewJobLogChange(e)}
+                         placeholder="输入今日工作事项"
+               />
+          </div>
+          <div className="col-md-12">
+            <button
+              className="btn btn-primary btn-lg"
+              onClick={e => this.handleNewJobLogSubmit(e)}
+            >
+              提交
+            </button>
+          </div>
+        </div>
+        <div className="col-md-12 jobLogs">
+          {this.state.isLoading && <span>Loading...</span>}
+          {this.state.jobLogs.map((jobLog) => (
+              <div className="logItem" key={jobLog.id}>
+                {jobLog.log} 创建于:{jobLog.created_at}
+              </div>
+            )
+          )}
+        </div>
       </div> : null
     );
   }
@@ -48,6 +132,11 @@ export default class Profile extends Component {
     const { userSession } = this.props;
     this.setState({
       person: new Person(userSession.loadUserData().profile),
+
     });
+  }
+
+  componentDidMount() {
+    this.fetchData()
   }
 }
